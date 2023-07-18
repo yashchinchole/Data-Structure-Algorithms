c82859c33c8825fa943cb9b84fee09a80596cecc
YASH <iamyash337@gmail.com>
2023-07-18 23:51:12 +0530

    main

--- 2Array/rotationMatrix.java
+++ 2Array/rotationMatrix.java
@@ -1,32 +1,6 @@
 import java.util.*;
 
 class rotationMatrix {
-    private static void rotatedMatrix(int[][] arr) {
-        int r = arr.length;
-        int c = arr[0].length;
-
-        for (int i = 0; i < r; i++) {
-            for (int j = i; j < c; j++) {
-                int temp = arr[i][j];
-                arr[i][j] = arr[j][i];
-                arr[j][i] = temp;
-            }
-        }
-
-        int li = 0;
-        int ri = c - 1;
-
-        for (int i = 0; i < r; i++) {
-            while (li < ri) {
-                int temp = arr[i][li];
-                arr[i][li] = arr[i][ri];
-                arr[i][ri] = temp;
-                li++;
-                ri--;
-            }
-        }
-    }
-
     public static void main(String[] args) {
         int[][] arr = {
                 { 1, 2, 3, 4 },
@@ -39,5 +13,29 @@ class rotationMatrix {
 
         for (var mat : arr)
             System.out.println(Arrays.toString(mat));
+    }
+
+    private static void rotatedMatrix(int[][] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr[0].length; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int leftIndex = 0;
+            int rightIndex = arr[0].length - 1;
+
+            while (leftIndex < rightIndex) {
+                int temp = arr[i][leftIndex];
+                arr[i][leftIndex] = arr[i][rightIndex];
+                arr[i][rightIndex] = temp;
+                leftIndex++;
+                rightIndex--;
+            }
+        }
     }
 }
