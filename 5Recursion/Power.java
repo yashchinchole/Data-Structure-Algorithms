9862385feebd4c4dcbc11068e989673cd9baaa71
YASH <iamyash337@gmail.com>
2023-08-17 01:59:12 +0530

    main

--- 5Recursion/Power.java
+++ 5Recursion/Power.java
@@ -1,27 +1,28 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Power {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long a = sc.nextInt();
-        long b = sc.nextInt();
+        BigInteger a = sc.nextBigInteger();
+        int b = sc.nextInt();
         System.out.println(power(a, b));
         sc.close();
     }
 
-    private static long power(long a, long b) {
-        long ans = 0, finalAns = 0;
+    private static BigInteger power(BigInteger a, int b) {
+        BigInteger ans, finalAns;
 
         if (b == 1)
             return a;
 
         else {
             ans = power(a, b / 2);
-            finalAns = ans * ans;
+            finalAns = ans.multiply(ans);
             if (b % 2 == 0)
                 return finalAns;
             else
-                return a * finalAns;
+                return a.multiply(finalAns);
         }
     }
 }
